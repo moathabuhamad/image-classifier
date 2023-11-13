c47e8846465f1f78eb279bb14cd5b7cc48ee332d
moathabuhamad <moathabuhamad@outlook.com>
2023-11-13 14:10:36 +0100

    new solution

--- predict.py
+++ predict.py
@@ -30,56 +30,70 @@ def process_image(image):
     returns an Numpy array
     """
 
+    # Process a PIL image for use in a PyTorch model
     im = Image.open(image)
+
+    # resize the image
     image_size = im.size
     width = image_size[0]
     height = image_size[1]
 
+    # im.thumbnail requires height and width to be tuple
     if width > height:
         im.thumbnail(
             (5000000, 256)
-        )
+        )  # used the arbitrary 5000000, but any large value will work
     else:
         im.thumbnail(
             (256, 5000000)
-        )
+        )  # used the arbitrary 5000000, but any large value will work
 
+    # crop out the center 224x224 portion of the image
     left_margin = (im.width - 224) / 2
     bottom_margin = (im.height - 224) / 2
     right_margin = left_margin + 224
     top_margin = bottom_margin + 224
 
+    # 4 turples coordinates
     im = im.crop((left_margin, bottom_margin, right_margin, top_margin))
+
+    # Get Numpy array image from a PIL image
     np_image = np.array(im)
 
+    # Normalization
     np_image = np_image / 255
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
     nor_image = (np_image - mean) / std
 
+    # PyTorch tensors assume the color channel is the first dimension
+    # turn a tensor of shape [width, height, color_channels] into [color_channels, width, hight]
     final_image = nor_image.transpose((2, 0, 1))
 
+    # returns an Numpy array
     return final_image
 
 
 def predict(image_path, model, topk):
     """Predict the class (or classes) of an image using a trained deep learning model."""
 
+    # Implement the code to predict the class from an image file
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.eval()
 
     model.to(
         device
-    )
+    )  # We should push the input data as well as the model onto the device
 
     image = process_image(image_path)
+    # process_image function returns a Numpy array, we need to Convert Numpy array to a PyTorch Tensor
     torch_image_tensor = torch.from_numpy(image).type(torch.FloatTensor)
     torch_image_tensor.unsqueeze_(
         0
-    )
+    )  # this needs to be added, to add a batch dimension to the input
     torch_image_tensor = torch_image_tensor.to(
         device
-    )
+    )  # The input tensor should be on the GPU!!!
 
     with torch.no_grad():
         log_ps = model(torch_image_tensor)
@@ -106,6 +120,7 @@ def load_checkpoint(filepath):
     model.class_to_idx = checkpoint["class_to_idx"]
     learning_rate = checkpoint["learning_rate"]
     optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)
+    # optimizer.load_state_dict(checkpoint['optimizer_dict'])
     optimizer.load_state_dict(checkpoint["optimizer"])
 
     return optimizer, model
@@ -129,6 +144,14 @@ def main():
 
     print(labels)
     print(probs)
+
+    # plt.subplot(2, 1, 1)
+    # plt.title(img_name)
+    # plt.imshow(check_image)
+
+    # plt.subplot(2, 1, 2)
+    # plt.barh(names, probs)
+    # plt.xlabel('Probabilities')
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -7,9 +7,13 @@ import numpy as np
 from PIL import Image
 from collections import OrderedDict
 
+# import argparse
 import argparse
 import sys
 
+# parser.add_arguement(--'', type= , default='', help='')
+
+# define command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("data_dir", action="store", default="./flowers/")
 parser.add_argument(
@@ -35,6 +39,8 @@ parser.add_argument("--epochs", type=int, default=10, help="enter number of epoc
 parser.add_argument("--gpu_cpu", type=str, default="cuda", help="specify cuda or cpu")
 args = parser.parse_args()
 
+# Model hyperparameters
+# assign parameters to passed arguments
 learning_rate = args.learning_rate
 hidden_units = args.hidden_units
 epochs = args.epochs
@@ -42,11 +48,13 @@ gpu_cpu = args.gpu_cpu
 arch = args.arch
 sav_dir = args.save_dir
 
+# Set data directory for image data
 data_dir = "flowers"
 train_dir = data_dir + "/train"
 valid_dir = data_dir + "/valid"
 test_dir = data_dir + "/test"
 
+# Define the transforms for the training, validation, and testing sets
 train_transforms = transforms.Compose(
     [
         transforms.RandomRotation(30),
@@ -72,18 +80,19 @@ test_transforms = transforms.Compose(
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]
 )
-
+# Load the datasets with ImageFolder
 train_data = datasets.ImageFolder(data_dir + "/train", transform=train_transforms)
 validation_data = datasets.ImageFolder(
     data_dir + "/valid", transform=validation_transforms
 )
 test_data = datasets.ImageFolder(data_dir + "/test", transform=test_transforms)
 
-
+# Using the image datasets and the trainforms, define the dataloaders
 trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
 validationloader = torch.utils.data.DataLoader(validation_data, batch_size=64)
 testloader = torch.utils.data.DataLoader(test_data, batch_size=64)
 
+# image_datasets is a dictionary which has train, validation and test datasets(with transforms)
 image_datasets = {
     "train": datasets.ImageFolder(data_dir + "/train", transform=train_transforms),
     "validation": datasets.ImageFolder(
@@ -92,19 +101,24 @@ image_datasets = {
     "test": datasets.ImageFolder(data_dir + "/test", transform=test_transforms),
 }
 
+# Import JSON file for category mapping
 import json
 
 with open("cat_to_name.json", "r") as f:
     cat_to_name = json.load(f)
 
-
+# Build and train the network
+# Use GPU if it's available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# BUILD THE MODEL
 model = getattr(models, args.arch)(pretrained=True)
 model
 
+# Freeze parameters so we don't backprop through them
 for param in model.parameters():
     param.requires_grad = False
 
+# Define new classifer
 if args.arch == "vgg16":
     classifier = nn.Sequential(
         OrderedDict(
@@ -130,35 +144,52 @@ elif args.arch == "densenet121":
         )
     )
 
+# Set the classifier part of the VGG model = classifier
 model.classifier = classifier
 criterion = nn.NLLLoss()
 
+# Only train the classifier parameters, feature parameters are frozen
+# Optimizers require the parameters to optimize and a learning rate
 optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
-
+# Move this model to available device
 model.to(device)
 
+# TRAIN THE MODEL
+print("Coffee Time!")
 print("Initializing Training...")
-
+# Tracking the train steps
 steps = 0
+# Tracking the loss
 running_loss = 0
+# How many steps we're going to go before printing out the validation loss
 print_every = 40
 
 for epoch in range(epochs):
+    # Training loop
     for images, labels in trainloader:
         steps += 1
 
+        # move images and labels to GPU
         images, labels = images.to(device), labels.to(device)
+        # We need to zero the gradients on each training pass or we'll retain gradients from previous training batches.
         optimizer.zero_grad()
+        # Forward pass,  get our logits
         logps = model(images)
+        # Calculate the loss with the logits and the labels
         loss = criterion(logps, labels)
+        # then backward pass, then update weights
         loss.backward()
+        # Take an update step and update the new weights
         optimizer.step()
 
         running_loss += loss.item()
+        # STEP 3 Validation loop
         if steps % print_every == 0:
+            # turn the model into evaluation mode (turn off dropout)
             model.eval()
             validation_loss = 0
             accuracy = 0
+            # Turn off gradients for validation, saves memory and computations
             with torch.no_grad():
                 for images, labels in validationloader:
                     images, labels = images.to(device), labels.to(device)
@@ -167,9 +198,11 @@ for epoch in range(epochs):
                     loss = criterion(logps, labels)
                     validation_loss += loss.item()
 
+                    # Calculate the accuracy
                     ps = torch.exp(logps)
                     top_ps, top_class = ps.topk(1, dim=1)
                     equality = top_class == labels.view(*top_class.shape)
+                    # Calculate accuracy from equality
                     accuracy += torch.mean(equality.type(torch.FloatTensor))
 
             print(
@@ -182,11 +215,13 @@ for epoch in range(epochs):
             running_loss = 0
             model.train()
 
+# TODO: Do validation on the test set
 
+# turn the model into evaluation mode (turn off dropout)
 model.eval()
 validation_loss = 0
 accuracy = 0
-
+# Turn off gradients for validation, saves memory and computations
 with torch.no_grad():
     for images, labels in testloader:
         images, labels = images.to(device), labels.to(device)
@@ -195,9 +230,11 @@ with torch.no_grad():
         loss = criterion(logps, labels)
         validation_loss += loss.item()
 
+        # Calculate the accuracy
         ps = torch.exp(logps)
         top_ps, top_class = ps.topk(1, dim=1)
         equality = top_class == labels.view(*top_class.shape)
+        # Calculate accuracy from equality
         accuracy += torch.mean(equality.type(torch.FloatTensor))
 
 print(
@@ -205,6 +242,10 @@ print(
     f"Validation accuracy: {accuracy/len(testloader):.3f}"
 )
 
+# Save the checkpoint
+#'learning_rate': args.learning_rate,
+# The parameters for PyTorch networks are stored in a model's state_dict.
+# And the state dict contains the weight and bias matrices for each of the layers.
 model.class_to_idx = image_datasets["train"].class_to_idx
 checkpoint = {
     "arch": args.arch,
